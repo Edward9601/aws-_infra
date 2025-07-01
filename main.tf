e24a4482b628493b0c1a65e7292606a54007880c
Edward <ayvari16@gmail.com>
2025-06-30 21:16:09 -0400

    update S3 configuration slightly to make bucket public

--- main.tf
+++ main.tf
@@ -26,10 +26,26 @@ resource "aws_s3_bucket" "recipe_media_bucket" {
 resource "aws_s3_bucket_public_access_block" "recipe_media_bucket" {
     bucket = aws_s3_bucket.recipe_media_bucket.id
 
-    block_public_acls       = true
-    block_public_policy     = true
-    ignore_public_acls      = true
-    restrict_public_buckets = true
+    block_public_acls       = false
+    block_public_policy     = false
+    ignore_public_acls      = false
+    restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "allow_public_read" {
+  bucket = aws_s3_bucket.recipe_media_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = "*"
+        Action = "s3:GetObject"
+        Resource = "${aws_s3_bucket.recipe_media_bucket.arn}/*"
+      }
+    ]
+  })
 }
 
 # Enable server-side encryption
